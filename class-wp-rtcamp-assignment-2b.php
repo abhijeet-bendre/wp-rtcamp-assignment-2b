34492eae7e4f5d81340dce03696839fb98174df9
abhijeet <abhijeet@tymescripts.com>
2017-09-23 19:32:19 +0530

    Setup and Render contributors metabox.

--- class-wp-rtcamp-assignment-2b.php
+++ class-wp-rtcamp-assignment-2b.php
@@ -9,7 +9,7 @@
 /*
 Plugin Name: Assignment-2b: WordPress-Contributors Plugin
 Plugin URI:  http://tymescripts.com/rtCamp-assignment
-Description: Assignment-2b: WordPress-Contributors Plugin
+Description: WordPress-Contributors Plugin, adds a contributor metabox box with names and their Gravatars to posts section. Selected contributors are shown in frontend for posts for which they have contributed.
 Version:     0.1
 Author:      Abhijeet Bendre
 Author URI:  http://tymescripts.com
@@ -40,9 +40,48 @@ class Wp_Rtcamp_Assignment_2b {
 	 * @since 0.1
 	 */
 	public function __construct() {
-		// Rolling in magic.
+		add_action( 'admin_init', array( $this, 'wprtc_setup_contributors_metabox' ) );
 	}
 
+	/**
+	 * Setup Metaboxes for contributors
+	 *
+	 * @since 0.1
+	 */
+	public function wprtc_setup_contributors_metabox() {
+		add_meta_box( 'wprtc_contributors',  __( 'Contributors', 'wprtc_assignment_2b' ), array( $this, 'wprtc_render_contributors_metabox' ), 'post', 'normal', 'low' );
+	}
+
+	/**
+	 * Render contributors metabox.
+	 *
+	 * @since 0.1
+	 */
+	public function wprtc_render_contributors_metabox() {
+		ob_start();
+		$all_users = get_users();
+		//var_dump($all_users);
+		if ( ! empty( $all_users ) ) {
+			echo "<table class='wprtc_contributors_table' cellspacing='0'>
+							<thead>
+								<tr>
+									<th class='wprtc_contributors_head' scope='col'></th>
+					 				<th class='wprtc_contributors_head' scope='col'>Author Name</th>
+					 				<th class='wprtc_contributors_head' scope='col'>Gravatar</th>
+								</tr>
+							</thead>
+							<tbody>";
+			foreach ( $all_users as $all_users_key => $all_users_value ) {
+				echo "<tr>
+        					<td class=''><input type='checkbox' name='_wprtc_contributor' value='" . esc_attr( $all_users_value->ID ) . "'></td>
+        					<td class=''>$all_users_value->user_login</td>
+									<td class=''>" . get_avatar( $all_users_value->ID, 32 ) . '</td>
+        			</tr>';
+			}
+			echo '</tbody></table>';
+			ob_get_flush();
+		}
+	}
 }
 
 new Wp_Rtcamp_Assignment_2b();

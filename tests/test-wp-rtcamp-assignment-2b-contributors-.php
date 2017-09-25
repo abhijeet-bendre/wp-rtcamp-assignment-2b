2d90057181a7a92860b6bb5fe5ae5f2910764689
abhijeet <abhijeet@tymescripts.com>
2017-09-25 18:05:16 +0530

    Test if Plugin is active.

--- tests/test-wp-rtcamp-assignment-2b-contributors-.php
+++ tests/test-wp-rtcamp-assignment-2b-contributors-.php
@@ -14,7 +14,7 @@ class Wp_Rtcamp_Assignment_2b_Contributors_Test extends WP_UnitTestCase {
 	 * Test if Plugin is active.
 	 */
 	function test_is_plugin_active() {
-		$this->assertTrue( is_plugin_active( WPRTC_2B_PLUGIN_NAME . '/' . WPRTC_2B_PLUGIN_NAME . 'php' ) );
+		$this->assertTrue( is_plugin_active( WPRTC_2B_CONTRIBUTORS_PLUGIN_NAME . '/' . WPRTC_2B_CONTRIBUTORS_PLUGIN_NAME . 'php' ) );
 	}
 
 }

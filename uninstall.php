b406a12ea2f3349ad452292603179dc332326924
abhijeet <abhijeet@tymescripts.com>
2017-10-04 16:14:04 +0530

    Fixed phpcs errors

--- uninstall.php
+++ uninstall.php
@@ -19,7 +19,7 @@ function wprtc_contributors_delete_plugin_post_meta() {
 	global $wpdb;
 
 	if ( defined( 'WPRTC_CONTRIBUTORS_DELETE_ALL_DATA' ) && true === WPRTC_CONTRIBUTORS_DELETE_ALL_DATA ) {
-			$wpdb->query( "DELETE FROM {$wpdb->postmeta}  WHERE  `meta_key` LIKE '_wprtc_contributors'" );
+			$wpdb->query( "DELETE FROM {$wpdb->postmeta}  WHERE  `meta_key` LIKE '_wprtc_contributors'" ); // db call ok; no-cache ok.
 	}
 
 }

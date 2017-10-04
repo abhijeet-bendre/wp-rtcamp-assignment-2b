5e847c4c713fcf3c5d26a41b3737c42e99fa1222
abhijeet <abhijeet@tymescripts.com>
2017-10-04 16:09:23 +0530

    Added uninstall.php to delete plugin data

---
+++ uninstall.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * WordPress-Contributors Plugin Uninstallation
+ *
+ * Uninstalling deletes '_wprtc_contributors' post meta.
+ *
+ * @package rtCamp
+ * @version 0.1
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+/**
+ * If WPRTC_CONTRIBUTORS_DELETE_ALL_DATA constant is set to true in wp-config.php then only delete the associated data.
+ */
+function wprtc_contributors_delete_plugin_post_meta() {
+	global $wpdb;
+
+	if ( defined( 'WPRTC_CONTRIBUTORS_DELETE_ALL_DATA' ) && true === WPRTC_CONTRIBUTORS_DELETE_ALL_DATA ) {
+			$wpdb->query( "DELETE FROM {$wpdb->postmeta}  WHERE  `meta_key` LIKE '_wprtc_contributors'" );
+	}
+
+}
+define( 'WPRTC_CONTRIBUTORS_DELETE_ALL_DATA', true );
+wprtc_contributors_delete_plugin_post_meta();
